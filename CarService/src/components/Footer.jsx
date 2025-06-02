f4b20ee3a699b6275b557a0aa8daa84d605fd4c4
Javier <javierrp94@gmail.com>
2025-06-02 11:52:26 +0200

    Añadir información del desarrollador en el pie de página

--- CarService/src/components/Footer.jsx
+++ CarService/src/components/Footer.jsx
@@ -2,6 +2,7 @@ function Footer() {
   return (
     <footer className="footer">
       <p>© 2025 CarService Popeye. Todos los derechos reservados.</p>
+      <p>Desarrollado por Javier Ruiz Porta</p>
     </footer>
   )
 }
